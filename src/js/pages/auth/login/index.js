a6a819898d5829d17593917b99f23164f181b4e3
IrinaZinchenko <zinahohol@gmail.com>
2024-01-04 16:30:57 +0300

    added styles to log-in form

--- src/js/pages/auth/login/index.js
+++ src/js/pages/auth/login/index.js
@@ -3,11 +3,17 @@ export const Login = () => {
   elem.classList.add('log-in');
 
   const loginTmp = `
-    <form>
-      <input id="username" type="text" placeholder="Введите имя">
-      <input id="password" type="password" placeholder="Введите пароль">
+    <form class="log-in-form">
+      <div class="form-field">
+        <label for="username">Имя пользователя</label>
+        <input id="username" name="username" type="text" placeholder="Введите имя">
+      </div>
+      <div class="form-field">
+        <label for="password">Пароль</label>
+        <input id="password" name="password" type="password" placeholder="Введите пароль">
+      </div>
 
-      <a href="/">Войти</a>
+      <a href="/" class="log-in-btn">Войти</a>
     </form>
   `;
 
